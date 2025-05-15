86edafa6b8ce60ac728d9d9c3be04faffe443afd
n2hq <n2hq.co@gmail.com>
2025-05-15 14:01:47 +0100

    updated nav links

--- app/components/header/Logo.tsx
+++ app/components/header/Logo.tsx
@@ -12,17 +12,24 @@ const Logo = ({ theme }: NavTheme) => {
     }, [theme])
     return (
         <div className='flex place-items-center space-x-1 relative'>
-            <div className={`${theme === "light" ? "bg-black text-white" : "bg-white/90 text-black"} 
-            md:w-9 md:h-9 w-8 h-8 rounded-full relative
-            flex justify-center flex-col`}>
-                <FaC className=' text-lg absolute left-1' />
-                <FaO className=' text-lg absolute right-1' />
-            </div>
-            <h1 className={`${theme === "light" ? "text-black" : "text-white/90"} 
+            <a href="/">
+                <div className={`${theme === "light" ? "bg-black text-white" : "bg-white/90 text-black"} 
+            md:w-9 md:h-9 w-8 h-8 rounded-full relative overflow-hidden
+            flex place-content-center place-items-center flex-col`}>
+                    <img
+                        src="images/comcerc-logo.png"
+                        alt="comcerc"
+                        className={`object-cover w-full h-full`}
+                    />
+                </div>
+            </a>
+            <a href="/">
+                <h1 className={`${theme === "light" ? "text-black" : "text-white/90"} 
             font-[700] text-2xl sm:text-2xl md:text-3xl tracking-tight relative top-[-1px]
             first-letter: font-sans first-letter: `}>
-                comcerc
-            </h1>
+                    comcerc
+                </h1>
+            </a>
         </div>
     )
 }
